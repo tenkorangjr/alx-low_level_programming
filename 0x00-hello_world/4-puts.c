1b620db7b84cab2a5bf50619132f5eb81ea697d7
Michael Ofori Tenkorang Jr <82475246+tenkorangjr@users.noreply.github.com>
2022-06-30 23:29:21 +0000

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *Description: main - Prints out a string to stdout
+ *comments in C source code.
+ *Please use it consistently.
+ *Return: 0 if success
+ *Description:  A column of asterisks on the left side,
+ *with beginning and ending almost-blank lines.
+ */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
