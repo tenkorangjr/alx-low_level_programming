ad4c6e88cf4c8fc7cfb93517126749496c4de228
Michael Ofori Tenkorang Jr <82475246+tenkorangjr@users.noreply.github.com>
2022-07-02 00:14:58 +0000

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>                                                                                                                              
+int main(){                                                                                                                                     
+                char pr;                                                                                                                        
+char ch;                                                                                                                                        
+                        for (ch = 'a';ch <= 'z'; ch++){                                                                                         
+                                                                                                                                                
+                        putchar(ch);                                                                                                            
+                                                                                                                                                
+                        }                                                                                                                       
+                                                                                                                                   
+                        for (pr = 'A';pr <= 'Z'; pr++){                                                                                         
+                                                                                                                                                
+                       putchar(pr);                                                                                                             
+                        }       return 0;                                                                                                       
+                                                                                                                                                
+} 
